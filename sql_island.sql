5666cc4a68e2930b876c18e72539367a9e8b90c9
Beatriz Moura <biamouras@gmail.com>
2023-02-28 11:22:57 -0300

    create file

---
+++ sql_island.sql
@@ -0,0 +1,6 @@
+/*
+https://sql-island.informatik.uni-kl.de/
+This is you!
+After the survived plane crash, you will be stuck on SQL Island for the time being.
+By making progress in the game, you will find a way to escape from this island.
+*/
