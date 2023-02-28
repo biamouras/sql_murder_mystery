5b8b83a52a9d42181fdcb1d8b1a06d2fad598cc8
Beatriz Moura <biamouras@gmail.com>
2023-02-28 11:05:55 -0300

    suspect

--- murder_mystery.sql
+++ murder_mystery.sql
@@ -39,19 +39,45 @@ TRANSCRIPT RESULT
 I heard a gunshot and then saw a man run out. 
 He had a "Get Fit Now Gym" bag. 
 The membership number on the bag started with "48Z". 
-Only gold members have those bags. The man got into a car with a plate that included "H42W".
+Only gold members have those bags. 
+The man got into a car with a plate that included "H42W".
 */
 
 /* get the interview of second witness */
 select a.*
 from interview a, person b
 where a.person_id = b.id
-	and b.name like 'Annabel%'
-	and b.address_street_name = 'Franklin Ave';
+	and b.name like 'Annabel%' /* person with the name Annabel */
+	and b.address_street_name = 'Franklin Ave'; /* that lives in the mentioned address */
 
 /*
 witness 2: 16371 (person id)
 TRANSCRIPT RESULT
 I saw the murder happen, and I recognized the killer from my gym 
 when I was working out last week on January the 9th.
+*/
+
+/* get the suspect */
+select a.id as person_id, a.name, b.id as member_id, 
+	b.membership_status, c.plate_number, d.check_in_date
+from person a, 
+	get_fit_now_member b,
+	drivers_license c,
+	get_fit_now_check_in d 
+where a.id = b.person_id /* join tables */
+	and a.license_id = c.id /* join tables */
+	and b.id like '48Z%' /* 1st witness: membership number on the bag started with "48Z" */
+	and b.membership_status = 'gold' /* 1st witness: gold members have those bags*/
+	and c.plate_number like '%H42W%' /* 1st witness: car with a plate that included "H42W" */
+	and b.id = d.membership_id /* join tables */
+	and d.check_in_date = 20180109; /* 2nd witness: January the 9th */
+
+/*
+SUSPECT
+person_id: 67318
+name: Jeremy Bowers
+member_id: 48Z55
+membership_status: gold
+plate_number: 0H42W2
+check_in_date: 20180109					
 */
