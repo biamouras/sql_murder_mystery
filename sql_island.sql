6b158723fd0081e9033171edc0bacfac55ac7480
Beatriz Moura <biamouras@gmail.com>
2023-02-28 12:09:37 -0300

    friendly inhabitants

--- sql_island.sql
+++ sql_island.sql
@@ -1,6 +1,26 @@
 /*
 https://sql-island.informatik.uni-kl.de/
-This is you!
-After the survived plane crash, you will be stuck on SQL Island for the time being.
-By making progress in the game, you will find a way to escape from this island.
+ME: Oh dear, what happened? It seems that I am the only survivor of the air crash. Wow, there are some villages on this island.
 */
+
+SELECT * FROM village
+
+/* ME: It seems there are a few people living in these villages. How can I see a list of all inhabitants? */
+SELECT * FROM inhabitant
+
+/* ME: Man! I'm hungry. I will go and find a butcher to ask for some free sausages.*/
+
+SELECT * FROM inhabitant WHERE job = 'butcher'
+
+/* 
+BUTCHER:
+    There you are! Enjoy your meal! But take care of yourself. 
+    As long as you are unarmed, stay away from villains. 
+    Not everyone on this island is friendly.
+ME:
+    Thank you, Edward! Okay, let's see who is friendly on this island...
+*/
+
+SELECT * 
+FROM inhabitant
+WHERE state = 'friendly';
